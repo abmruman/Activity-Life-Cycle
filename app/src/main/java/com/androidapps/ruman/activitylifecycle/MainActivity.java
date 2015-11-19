bc9b76f208b05641b9410e1cc8dd90486458a9c7
A B M Ruman <abm.ruman@gmail.com>
2015-11-20 03:01:34 +0600

    Make Toast to get notified when each method is triggered.

--- app/src/main/java/com/androidapps/ruman/activitylifecycle/MainActivity.java
+++ app/src/main/java/com/androidapps/ruman/activitylifecycle/MainActivity.java
@@ -2,11 +2,13 @@ package com.androidapps.ruman.activitylifecycle;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onContentChanged() {
+        Toast.makeText(MainActivity.this, "onContentChanged", Toast.LENGTH_SHORT).show();
         super.onContentChanged();
     }
 
@@ -14,50 +16,60 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this, "onCreate", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
+        Toast.makeText(MainActivity.this, "onRestart", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        Toast.makeText(MainActivity.this, "onStart", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        Toast.makeText(MainActivity.this, "onResume", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onResumeFragments() {
         super.onResumeFragments();
+        Toast.makeText(MainActivity.this, "onResumeFragments", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onPostResume() {
         super.onPostResume();
+        Toast.makeText(MainActivity.this, "onPostResume", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onBackPressed() {
+        Toast.makeText(MainActivity.this, "onBackPressed", Toast.LENGTH_SHORT).show();
         super.onBackPressed();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        Toast.makeText(MainActivity.this, "onPause", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onStop() {
+        Toast.makeText(MainActivity.this, "onStop", Toast.LENGTH_SHORT).show();
         super.onStop();
     }
 
     @Override
     protected void onDestroy() {
+        Toast.makeText(MainActivity.this, "onDestroy", Toast.LENGTH_SHORT).show();
         super.onDestroy();
     }
 
