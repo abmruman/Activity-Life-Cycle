4783b1fbfc924423f70d5a724740ab6fb91a0bb1
A B M Ruman <abm.ruman@gmail.com>
2015-11-20 03:00:25 +0600

    Override activity life cycle callback methods.

--- app/src/main/java/com/androidapps/ruman/activitylifecycle/MainActivity.java
+++ app/src/main/java/com/androidapps/ruman/activitylifecycle/MainActivity.java
@@ -6,8 +6,59 @@ import android.support.v7.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    public void onContentChanged() {
+        super.onContentChanged();
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
 }
